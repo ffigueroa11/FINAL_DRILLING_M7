424f0e38e1cb4a816a2e4b1e11453026f9918dc0
Francisco Figueroa <francisco.figueroa@hotmail.cl>
2025-01-14 21:23:37 -0300

    pruebas unitarias

--- laboratorio/admin.py
+++ laboratorio/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Laboratorio, DirectorGeneral, Producto
 
-# Configuración para mostrar los modelos de manera adecuada
+
 @admin.register(Laboratorio)
 class LaboratorioAdmin(admin.ModelAdmin):
     list_display = ('id', 'nombre')
